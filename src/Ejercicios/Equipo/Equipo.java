f4e7032df324ee32346605258aacf96e40937098
Daniel-Barrientos <danielstivenbs@ufps.edu.co>
2023-04-01 23:52:38 -0500

    Ejercicio Equipo - Terminnado

--- src/Ejercicios/Equipo/Equipo.java
+++ src/Ejercicios/Equipo/Equipo.java
@@ -55,20 +55,48 @@ public class Equipo {
         return null;
     }
 
-    public void buscarMayorPuntaje() {
+    public String buscarMayorPuntaje() {
         Node<Jugador> actual = jugadores.getInicio();
+        Integer puntaje = 0;
         Node<Jugador> mayor = null;
 
         while (actual != null) {
-            if (actual.getDato().getPuntos() > mayor.getDato().getPuntos()) {
+            if (actual.getDato().getPuntos() > puntaje) {
                 mayor = actual;
                 actual = actual.getSiguiente();
             }
         }
-        System.out.println("Jugador: " + mayor.getDato().getNombre() + ". Edad: " + mayor.getDato().getEdad());
+        return ("Jugador: " + mayor.getDato().getNombre() + ". Edad: " + mayor.getDato().getEdad() + ". Puntaje: " + mayor.getDato().getPuntos());
     }
 
+    public void aumentarPuntos(int puntos) {
+        Node<Jugador> aux = jugadores.getInicio();
+        
+        while (aux != null) {
+            Integer tmp = aux.getDato().getPuntos() + puntos;
+            aux.getDato().setPuntos(tmp);
+            aux = aux.getSiguiente();
+        }
+    }
     
+    
+    //se busca jugadores con determinada edad
+    public ArrayList<Jugador> buscarJugadorPorEdad(int min, int max) {
+        Node<Jugador> actual = jugadores.getInicio();
+        ArrayList<Jugador> jugadoresEdad = new ArrayList();
+        int cont = 0;
+
+        while (actual != null) {
+            if (actual.getDato().getEdad() >= min && actual.getDato().getEdad() <= max) {
+                jugadoresEdad.add(actual.getDato());
+                cont += 1;
+            } 
+            actual = actual.getSiguiente();
+        }
+        System.out.println("Hay " + cont + " jugadores con edad entre " + min + " y " + max);
+        return jugadoresEdad;
+    }
+
     //Se imprime la lista de jugadores de la lista doble
     public void imprimirJugador() {
         Node<Jugador> aux = jugadores.getInicio();
@@ -79,7 +107,6 @@ public class Equipo {
         }
     }
 
-    
     //metodo generico para imprimir listas de ArrayList
     public void imprimiArrayList(ArrayList<Jugador> jugadores) {
 
--- src/Ejercicios/Equipo/Test.java
+++ src/Ejercicios/Equipo/Test.java
@@ -15,7 +15,7 @@ public class Test {
         equipo.addJugador(new Jugador("Isaac", 20, 1.75, "124", 10));
         equipo.addJugador(new Jugador("Harold", 20, 1.63, "125", 4));
         equipo.addJugador(new Jugador("Moises", 26, 1.80, "126", 1));
-        equipo.addJugador(new Jugador("Jaider", 23, 1.60, "152", 12));
+        equipo.addJugador(new Jugador("Jaider", 23, 1.60, "152", 10));
         equipo.addJugador(new Jugador("Yofer", 21, 1.68, "136", 6));
         equipo.imprimirJugador();
         
@@ -26,6 +26,19 @@ public class Test {
         System.out.println("\n-------------");
         System.out.println("Se buscan un jugador por cedula");
         System.out.println(equipo.buscarJugador("124"));
+        
+        System.out.println("\n-------------");
+        System.out.println("se muestra el jugador de mayor puntaje");
+        System.out.println(equipo.buscarMayorPuntaje());
+        
+        System.out.println("\n-------------");
+        System.out.println("Se aumenta 5 puntos a todos los jugadores");
+        equipo.aumentarPuntos(5);
+        equipo.imprimirJugador();
+        
+        System.out.println("\n-------------");
+        System.out.println("Se busca jugadores entre 20 y 25 anios");
+        equipo.imprimiArrayList(equipo.buscarJugadorPorEdad(20, 25));
         
         
     }
